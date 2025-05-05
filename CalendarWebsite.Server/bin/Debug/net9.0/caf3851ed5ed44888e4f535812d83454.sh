3a87b75b3a6ad8b97a6969570ea337891a9930f8
Ewad245 <89444542+Ewad245@users.noreply.github.com>
2025-05-05 15:23:57 +0700

    Up

---
+++ CalendarWebsite.Server/bin/Debug/net9.0/caf3851ed5ed44888e4f535812d83454.sh
@@ -0,0 +1,30 @@
+function list_child_processes () {
+    local ppid=$1;
+    local current_children=$(pgrep -P $ppid);
+    local local_child;
+    if [ $? -eq 0 ];
+    then
+        for current_child in $current_children
+        do
+          local_child=$current_child;
+          list_child_processes $local_child;
+          echo $local_child;
+        done;
+    else
+      return 0;
+    fi;
+}
+
+ps 16392;
+while [ $? -eq 0 ];
+do
+  sleep 1;
+  ps 16392 > /dev/null;
+done;
+
+for child in $(list_child_processes 16406);
+do
+  echo killing $child;
+  kill -s KILL $child;
+done;
+rm /Users/thomasjohnson/Documents/GitHub/CalendarWebsiteFork/CalendarWebsite.Server/bin/Debug/net9.0/caf3851ed5ed44888e4f535812d83454.sh;
