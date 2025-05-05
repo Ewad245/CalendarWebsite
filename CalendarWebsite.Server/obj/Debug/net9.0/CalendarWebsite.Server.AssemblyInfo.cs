4127d1c0253d6ea9cb1fe775487a5706e1ec0c67
Ewad245 <89444542+Ewad245@users.noreply.github.com>
2025-05-05 15:23:57 +0700

    Up

--- CalendarWebsite.Server/obj/Debug/net9.0/CalendarWebsite.Server.AssemblyInfo.cs
+++ CalendarWebsite.Server/obj/Debug/net9.0/CalendarWebsite.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalendarWebsite.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c41372a11975ca380e1c0de03f40990e36901319")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdc8514a52d4d0f6038e06dc73462eafc5bfb7cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalendarWebsite.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalendarWebsite.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
