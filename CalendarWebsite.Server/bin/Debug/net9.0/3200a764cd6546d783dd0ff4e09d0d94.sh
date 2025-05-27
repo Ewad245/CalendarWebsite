3735af5a16d5fd01b58313495f8683d16b6a2023
Ewad245 <89444542+Ewad245@users.noreply.github.com>
2025-05-27 14:30:23 +0700

    Fix login issues

---
+++ CalendarWebsite.Server/bin/Debug/net9.0/3200a764cd6546d783dd0ff4e09d0d94.sh
@@ -0,0 +1,30 @@
+function list_child_processes () {
+    local ppid=$1;
+    local current_children=$(pgrep -P $ppid);
+    local local_child;
+    if [ $? -eq 0 ];
+    then
+        for current_child in $current_children
+        do
+          local_child=$current_child;
+          list_child_processes $local_child;
+          echo $local_child;
+        done;
+    else
+      return 0;
+    fi;
+}
+
+ps 51411;
+while [ $? -eq 0 ];
+do
+  sleep 1;
+  ps 51411 > /dev/null;
+done;
+
+for child in $(list_child_processes 51422);
+do
+  echo killing $child;
+  kill -s KILL $child;
+done;
+rm /Users/thomasjohnson/Documents/GitHub/CalendarWebsiteFork/CalendarWebsite.Server/bin/Debug/net9.0/3200a764cd6546d783dd0ff4e09d0d94.sh;
--- CalendarWebsite.Server/obj/Debug/net9.0/CalendarWebsite.Server.AssemblyInfo.cs
+++ CalendarWebsite.Server/obj/Debug/net9.0/CalendarWebsite.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalendarWebsite.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+858faf145156f9aa79686c6307a15d56b018ff7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5443a8d0dc9fe168f0abed2a867f4136671b9af7")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalendarWebsite.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalendarWebsite.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
