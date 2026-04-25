e33669c007c65c61f7a046e44051ebb1a2fb96ff
Ewad245 <89444542+Ewad245@users.noreply.github.com>
2026-04-25 10:08:57 +0700

    Remove [Authorize] attributes from controllers
    Removed the [Authorize] attribute from AuthController.GetUser and the class-level attributes on DataOnly_APIaCheckInController, DepartmentController, GenerateExcelReportController, and PositionController. This change allows these endpoints to be accessed without the attribute-based authorization (e.g., to allow anonymous access or to centralize auth enforcement in middleware/filters). No other logic was modified.

--- CalendarWebsite.Server/Controllers/GenerateExcelReportController.cs
+++ CalendarWebsite.Server/Controllers/GenerateExcelReportController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CalendarWebsite.Server.Controllers;
-[Authorize]
+
 [Route("api/[controller]")]
     [ApiController]
     public class GenerateExcelReport : ControllerBase
